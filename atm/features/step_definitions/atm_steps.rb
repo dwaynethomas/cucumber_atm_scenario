74e56da5caef1514a5016c4247e69d84bd3d579a
Dwayne Thomas <thomas.dwayne@gmail.com>
2016-01-04 07:09:26 -0800

    first step should be passing but isn't, finding ruby errors unhelpful

--- atm/features/step_definitions/atm_steps.rb
+++ atm/features/step_definitions/atm_steps.rb
@@ -1,3 +1,9 @@
+module KnowsMyAccount
+  def my_account
+    @my_account || Account.new
+  end
+end
+
 class Account
   def deposit(amount)
     @balance = amount
@@ -21,15 +27,13 @@ CAPTURE_CASH_AMOUNT = Transform /^(£|\$|€)(\d+)$/ do |currency_symbol, digits
 end
 
 Given /^I have deposited \$(#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
-  @my_account = Account.new
-  @my_account.deposit(amount)
-  @my_account.balance.should eq(amount)
-   "Expected the balance to be #{amount} but it was #{my_account.balance}"
+  my_account.deposit(amount)
+  my_account.balance.should eq(amount),"Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
 
 When /^I withdraw (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
   teller = Teller.new
-  teller.withdraw_from(@my_account, amount) # express the regexp above with the code you wish you had
+  teller.withdraw_from(my_account, amount) # express the regexp above with the code you wish you had
 end
 
 Then /^\$(\d+) should be dispensed.$/ do |arg1|
